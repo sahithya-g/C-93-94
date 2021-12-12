29492b56075c4d1cf36f134fb070cf2a48e53635
sahithya-g <79317689+sahithya-g@users.noreply.github.com>
2021-12-12 10:45:28 +0530

    Add files via upload

---
+++ kwitterroom.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCzUlGXgEVotRTrCMR9BvWc6CEdoRCbxso",
+  authDomain: "practiceactivityc94-b3de8.firebaseapp.com",
+  databaseURL: "https://practiceactivityc94-b3de8-default-rtdb.firebaseio.com",
+  projectId: "practiceactivityc94-b3de8",
+  storageBucket: "practiceactivityc94-b3de8.appspot.com",
+  messagingSenderId: "974578832183",
+  appId: "1:974578832183:web:49977ce2d0821fc5aa80b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
